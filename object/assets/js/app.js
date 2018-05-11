d48c311370e5e936d9a09c0b7d36926f539e88e1
kyou0 <clannad.zero@gmail.com>
2018-05-11 15:29:32 +0900

    3時29分コミット

--- object/assets/js/app.js
+++ object/assets/js/app.js
@@ -1,3 +1,29 @@
 /**
 買い物かごをJavaScriptオブジェクトで再現する
 **/
+
+var items = [
+    {
+        id: 1,
+        name: 'ノート',
+        image: 'note.png',
+        price: 150
+    },
+    {
+        id: 2,
+        name: 'ペン',
+        image: 'pen.png',
+        price: 300
+    },
+    {
+        id: 3,
+        name: '消しゴム',
+        image: 'eraser.png',
+        price: 1000
+    }
+];
+
+var cart = {
+    total_quantity: 0,
+    total_price: 0,
+};
