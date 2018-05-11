a35a6fa299a12a82c2d13cbca51769a71bbb60f0
kyou0 <clannad.zero@gmail.com>
2018-05-11 16:02:16 +0900

    Cart Add

--- object/assets/js/app.js
+++ object/assets/js/app.js
@@ -24,6 +24,35 @@ var items = [
 ];
 
 var cart = {
+    //合計数
     total_quantity: 0,
+    //合計金額
     total_price: 0,
+    //カートに入っている商品一覧
+    items: [],
+    //カートに商品を追加する
+    add: function (item, quantity) {
+        console.log("item", item);
+        console.log("quantity", quantity);
+
+        item.quantity = quantity;
+
+        this.items.push(item);
+    },
+    //カートの商品を削除する
+    remove: function () {
+
+    },
+    //カートの商品の数量を変更する
+    changeQuantity: function () {
+
+    },
+    //合計金額を計算する
+    calcTotalPrice: function () {
+
+    },
+    //合計数を計算する
+    calcTotalQuantity: function () {
+
+    }
 };
