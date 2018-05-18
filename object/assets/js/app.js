8b6e37935465c3b9253a15f7782ef03c567638c4
kyou0 <clannad.zero@gmail.com>
2018-05-18 14:58:24 +0900

    Test

--- object/assets/js/app.js
+++ object/assets/js/app.js
@@ -47,26 +47,72 @@ var cart = {
             _item = Object.assign({},item, {quantity: quantity});
 
             this.items.push(_item);
+        }else {
+
+            //既に商品があれば、その商品の数量に1を追加する
+            _item.quantity += quantity;
         }
-    },
-    //カートの商品を削除する
-    remove: function () {
+
+        //合計金額を計算して代入する
+        this.total_price = this.calcTotalPrice();
+        //合計数を計算して代入する
+        this.total_quantity = this.calcTotalQuantity();
+
+        return this;
 
     },
+    //カートの商品を削除する
+    remove: function (id) {
+
+        var index = this.items.findIndex(function (item) { return item.id === id });
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
+
+        this.total_price = this.calcTotalPrice();
+        this.total_quantity = this.calcTotalQuantity();
+
+        return this;
+    },
     //カートの商品の数量を変更する
-    changeQuantity: function () {
+    changeQuantity: function (id, quantity) {
+
+        var item = this.getItem(id);
+
+        if (item) {
+            if (quantity <= 0) {
+                this.remove(item.id);
+            }else{
+                item.quantity = quantity;
+
+                this.total_price = this.calcTotalPrice();
+                this.total_quantity = this.calcTotalQuantity();
+            }
+        }
+
+        return this;
 
     },
     //合計金額を計算する
     calcTotalPrice: function () {
 
+        var total = 0;
+        for (var i = 0; i < this.items.length; i++) {
+            total += this.items[i].price * this.items[i].quantity;
+        }
+
+        return total;
     },
     //合計数を計算する
     calcTotalQuantity: function () {
-
+        var total = 0;
+        for (var i = 0; i < this.items.length; i++) {
+            total += this.items[i].quantity;
+        }
+        return total;
     },
     //商品を取得する
-    getItem: function () {
+    getItem: function (id) {
         return this.items.find(function (item) {
             return item.id === id
         });
